3f3b25d374a3fea6c63578a634758c710edaf492
fabrichgit <fabrichjean@gmail.com>
2024-10-17 18:39:54 +0300

    feat: integrate user controller to route handler (2)

--- routes/user.route.go
+++ routes/user.route.go
@@ -14,6 +14,8 @@ func Userhandler(res http.ResponseWriter, req *http.Request) {
 		controller.GetAllUsers(res, req)
 	case path == "/register" && req.Method == http.MethodPost:
 		controller.Register(res, req)
+	case path == "/login" && req.Method == http.MethodPost:
+		controller.Login(res, req)
 	default:
 		http.Error(res, "Invalid request method", http.StatusMethodNotAllowed)
 	}
