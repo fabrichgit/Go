7d5c779090c2396ae5446ed3331fb36f6e3cccf2
fabrichgit <fabrichjean@gmail.com>
2024-10-17 19:18:13 +0300

    fixing bugs ...

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -17,51 +17,72 @@ func GetAllUsers(res http.ResponseWriter, req *http.Request) {
 
 func GetOneUsers(res http.Response, req *http.Request) {}
 
-type UserAuth struct {
+type AuthRequest struct {
 	name     string
 	password string
 }
 
-type TokenPayload struct {
-	token string
-}
-
 func Login(res http.ResponseWriter, req *http.Request) {
-	body, err := io.ReadAll(req.Body)
-
+	var loginReq AuthRequest
+	err := json.NewDecoder(req.Body).Decode(&loginReq)
 	if err != nil {
-		http.Error(res, "", http.StatusBadRequest)
-		return
-	}
-
-	defer req.Body.Close()
-
-	var user UserAuth
-	err = json.Unmarshal(body, &user)
-
-	if err != nil {
-		http.Error(res, "", http.StatusBadRequest)
+		http.Error(res, "Error parsing", http.StatusBadRequest)
 		return
 	}
 
 	var userFound = helper.FindUser(data.Users, func(u models.User) bool {
-		return u.Name == user.name
+		return u.Name == loginReq.name
 	})
+	json.NewEncoder(res).Encode(userFound)
 
-	if userFound == nil {
-		http.Error(res, "", http.StatusNotFound)
-		return
-	}
+	// if userFound == nil || !helper.CheckPasswordHash(loginReq.password, userFound.Password) {
+	// 	http.Error(res, "", http.StatusNotFound)
+	// 	return
+	// }
 
-	if helper.CheckPasswordHash(user.password, userFound.Password) {
-		token := TokenPayload{
-			token: helper.GenerateJWT(userFound.ID),
-		}
-		json.NewEncoder(res).Encode(token)
-		return
-	}
+	// token := struct {
+	// 	token string
+	// }{
+	// 	token: helper.GenerateJWT(userFound.ID),
+	// }
+	// res.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(res).Encode(token)
 
-	http.Error(res, "", http.StatusBadRequest)
+	// body, err := io.ReadAll(req.Body)
+
+	// if err != nil {
+	// 	http.Error(res, "", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// defer req.Body.Close()
+
+	// var user UserAuth
+	// err = json.Unmarshal(body, &user)
+
+	// if err != nil {
+	// 	http.Error(res, "", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// var userFound = helper.FindUser(data.Users, func(u models.User) bool {
+	// 	return u.Name == user.name
+	// })
+
+	// if userFound == nil {
+	// 	http.Error(res, "", http.StatusNotFound)
+	// 	return
+	// }
+
+	// if helper.CheckPasswordHash(user.password, userFound.Password) {
+	// 	token := TokenPayload{
+	// 		token: helper.GenerateJWT(userFound.ID),
+	// 	}
+	// 	json.NewEncoder(res).Encode(token)
+	// 	return
+	// }
+
+	// http.Error(res, "", http.StatusBadRequest)
 }
 
 func Register(res http.ResponseWriter, req *http.Request) {
@@ -83,6 +104,7 @@ func Register(res http.ResponseWriter, req *http.Request) {
 	}
 
 	user.ID = uuid.New().String()
+	user.Password = helper.HashPassword(user.Password)
 
 	data.Users = append(data.Users, user)
 
